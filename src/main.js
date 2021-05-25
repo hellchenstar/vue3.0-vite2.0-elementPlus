ab3d673c3f31bfc5b4c026c80fb168c98ca71cef
hell <x411520812@163.com>
2021-05-25 21:33:48 +0800

    ..

--- src/main.js
+++ src/main.js
@@ -14,7 +14,14 @@ const app = createApp(App)
 
 app.use(store)
 app.use(router)
+
 app.config.globalProperties.$ELEMENT = { size: 'small' }
+// 全局 错误处理
+app.config.errorHandler = (err, vm, info) => {
+  console.log(err)
+  console.log(vm)
+  console.log(info)
+}
 
 element(app);
 app.mount('#app')
