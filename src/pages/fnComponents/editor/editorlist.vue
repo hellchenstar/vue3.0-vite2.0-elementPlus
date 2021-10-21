a59508393f4f4de66a098b922d4ab4d9253fd46a
陈星 <x411520812@163.com>
2021-10-21 18:25:44 +0800

    更新富文本编辑器

--- src/pages/fnComponents/editor/editorlist.vue
+++ src/pages/fnComponents/editor/editorlist.vue
@@ -1,27 +1,61 @@
 <!--
  * @Author: chenx
  * @CreatedDate: Do not edit
- * @LastEditTime: 2021-10-21 17:22:25
+ * @LastEditTime: 2021-10-21 17:48:51
  * @Description: file content
 -->
 <template>
-	<div class="module">
-		<div>
-			富文本编辑器
-		</div>
-	</div>
+  <div class="module">
+    <div>
+      <h3>
+        vue-quill-editor编辑器<a
+          href="https://www.kancloud.cn/liuwave/quill/1409423"
+          >https://www.kancloud.cn/liuwave/quill/1409423</a
+        >
+      </h3>
+      <div>
+        <quill-editor
+          v-model="content"
+          ref="myQuillEditor"
+          @blur="onEditorBlur($event)"
+          @focus="onEditorFocus($event)"
+          @ready="onEditorReady($event)"
+        >
+        </quill-editor>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import { reactive } from "vue"
+// require styles
+import "quill/dist/quill.core.css"
+import "quill/dist/quill.snow.css"
+import "quill/dist/quill.bubble.css"
+import { quillEditor } from "vue-quill-editor"
 
 export default {
-	setup() {
-		const state = reactive({
-			content: "",
-		})
-
-		return { state }
-	},
+  components: {
+    quillEditor
+  },
+  setup() {
+    const state = reactive({
+      content: ""
+    })
+    const onEditorBlur = e => {
+      console.log(e)
+			state.content = "我有焦点了"
+    }
+    const onEditorFocus = e => {
+      console.log(e)
+			state.content = "失去焦点了"
+    }
+    const onEditorReady = e => {
+      console.log(e)
+			state.content = "准备好了"
+    }
+    return { state, onEditorBlur, onEditorFocus, onEditorReady }
+  }
 }
 </script>
