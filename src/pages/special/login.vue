6de05c9b8a323893bc6a00880a8a98a9ad4e56da
地狱咆哮 <hell@192.168.124.13>
2022-02-21 23:19:49 +0800

    ..

--- src/pages/special/login.vue
+++ src/pages/special/login.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: chenx
  * @CreatedDate: Do not edit
- * @LastEditTime: 2022-02-21 21:30:13
+ * @LastEditTime: 2022-02-21 23:19:27
  * @Description: 登录
 -->
 <template>
@@ -66,7 +66,8 @@ export default {
 							router.push("home")
 						})
 						.catch((err) => {
-							state.loginLoading = false
+								state.loginLoading = false
+							
 							console.log(err);
 						})
 					//
